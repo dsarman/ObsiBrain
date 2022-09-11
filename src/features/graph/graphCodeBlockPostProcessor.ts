125c6c235d8329aad1cb6cfaa78b95e9e2ef62d2
David Šarman <contact@sarman.work>
2022-09-11 10:57:40 +0200

    Remove unused dependencies

--- src/features/graph/graphCodeBlockPostProcessor.ts
+++ src/features/graph/graphCodeBlockPostProcessor.ts
@@ -1,15 +1,6 @@
 import { MarkdownPostProcessorContext } from 'obsidian';
-import { DataArray, DataviewApi, getAPI } from 'obsidian-dataview';
+import { getAPI } from 'obsidian-dataview';
 import { sb } from '../../common/loggingUtils';
-import { DataViewPage } from '../../common/types';
-import { IGraph, INode, ITask } from './react/graphTypes';
-import {
-  isComplete,
-  isFocused,
-  processField,
-} from '../../common/dataviewUtils';
-import { createRoot } from 'react-dom/client';
-import { Graph } from './react/Graph';
 import { ReactRenderChild } from '../../common/reactUtils';
 
 export const graphCodeBlockPostProcessor = (
